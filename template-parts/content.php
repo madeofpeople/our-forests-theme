90b27105d8a9f107ae038a6da693ce08d7d3e106
Pea <pea@misfist.com>
2022-07-15 20:14:24 -0400

    Modify template to only render title on single page/posts

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,9 +13,9 @@
 
 		<header class="entry-header">
 			<?php
-			if ( is_single() ) :
+			if ( is_singular() ) :
 				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
+			elseif( ! is_front_page() && ! is_home() ) :
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			endif;
 
