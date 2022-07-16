f16cc49f7f1d99ec54ab0b674237cafad53def42
Pea <pea@misfist.com>
2022-07-15 20:54:31 -0400

    Conditionally display by post format

--- index.php
+++ index.php
@@ -36,7 +36,11 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				if ( 'post' === get_post_type() ) :
+					get_template_part( 'template-parts/content', get_post_format() );
+				else :
+					get_template_part( 'template-parts/content', get_post_type() );
+				endif;
 
 			endwhile;
 
