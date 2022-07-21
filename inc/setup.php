d05fd7b6875d5ecd10f54de7bd29b0385669218b
Pea <pea@misfist.com>
2022-07-20 20:01:00 -0400

    Disable custom background

--- inc/setup.php
+++ inc/setup.php
@@ -68,18 +68,6 @@ function the_territory_setup() {
 		)
 	);
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'the_territory_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
 	// Custom logo support.
 	add_theme_support(
 		'custom-logo',
