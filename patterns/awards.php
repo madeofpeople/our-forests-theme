72b161114f24fc6aa1d002386daa5ee681a085da
Pea <pea@misfist.com>
2022-07-27 03:26:53 -0400

    Add patterns

---
+++ patterns/awards.php
@@ -0,0 +1,31 @@
+<?php
+ /**
+  * Title: Awards
+  * Slug: the-territory/awards
+  * Categories: featured, layout, gallery
+  */
+?>
+
+
+<!-- wp:gallery {"columns":5,"imageCrop":false,"linkTo":"none","className":"awards"} -->
+<figure class="wp-block-gallery has-nested-images columns-5 awards"><!-- wp:image {"id":450,"sizeSlug":"large","linkDestination":"none"} -->
+    <figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_theme_file_uri( 'build/images/placeholders/cph-dox.png' ) ); ?>" alt="" class="wp-image-450"/></figure>
+    <!-- /wp:image -->
+
+    <!-- wp:image {"id":454,"sizeSlug":"large","linkDestination":"none"} -->
+    <figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_theme_file_uri( 'build/images/placeholders/full-frame.png' ) ); ?>" alt="" class="wp-image-454"/></figure>
+    <!-- /wp:image -->
+
+    <!-- wp:image {"id":452,"sizeSlug":"large","linkDestination":"none"} -->
+    <figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_theme_file_uri( 'build/images/placeholders/sundance-2.png' ) ); ?>" alt="" class="wp-image-452"/></figure>
+    <!-- /wp:image -->
+
+    <!-- wp:image {"id":451,"sizeSlug":"large","linkDestination":"none"} -->
+    <figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_theme_file_uri( 'build/images/placeholders/sundance.png' ) ); ?>" alt="" class="wp-image-451"/></figure>
+    <!-- /wp:image -->
+
+    <!-- wp:image {"id":453,"sizeSlug":"large","linkDestination":"none"} -->
+    <figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_theme_file_uri( 'build/images/placeholders/true-false.png' ) ); ?>" alt="" class="wp-image-453"/></figure>
+    <!-- /wp:image -->
+</figure>
+<!-- /wp:gallery -->
---
+++ patterns/testimonial.php
@@ -0,0 +1,25 @@
+<?php
+ /**
+  * Title: Actions Count
+  * Slug: the-territory/action-count
+  * Categories: featured, layout
+  */
+?>
+
+<!-- wp:site-functionality/tout {"className":"tout parallax-text-overlay tout-testimonial"} -->
+<!-- wp:nk/awb {"type":"image","image":129,"imageTag":"%3Cimg%20src%3D%22<?php echo esc_url( get_theme_file_uri( 'build/images/backgrounds/parallax.jpg' ) ); ?>%22%20class%3D%22wp-image-129%20jarallax-img%22%20width%3D%224096%22%20height%3D%222160%22%20%2F%3E","backgroundColor":"rgba(128, 173, 108, 0.25)","parallax":"scroll","parallaxSpeed":0.25,"parallaxMobile":true,"ghostkitStyles":{},"ghostkitClassname":"nk-awb-1tlk8J","ghostkitId":"1tlk8J"} -->
+<div class="wp-block-nk-awb nk-awb">
+    <div class="nk-awb-wrap" style="background-color:rgba(128, 173, 108, 0.25)" data-awb-type="image" data-awb-image-background-size="cover" data-awb-image-background-position="50% 50%" data-awb-parallax="scroll" data-awb-parallax-speed="0.25" data-awb-parallax-mobile="true">
+        <div class="nk-awb-inner">
+            <img src="<?php echo esc_url( get_theme_file_uri( 'build/images/backgrounds/parallax.jpg' ) ); ?>" class="wp-image-129 jarallax-img" width="4096" height="2160" />
+        </div>
+    </div>
+    <!-- wp:quote {"textColor":"white","className":"is-style-plain"} -->
+    <blockquote class="wp-block-quote is-style-plain has-white-color has-text-color">
+        <p><?php esc_html_e( '"historically, our existence has been marginalized and erased. through this film we\'re changing that."', 'the-territory' ); ?></p>
+        <cite><?php esc_html_e( 'Bitaté Uru-eu-wau-wau', 'the-territory' ); ?></cite>
+    </blockquote>
+    <!-- /wp:quote -->
+</div>
+<!-- /wp:nk/awb -->
+<!-- /wp:site-functionality/tout -->
