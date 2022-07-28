2e07b66bfd9a4b2db60e367eae594354f84972bf
Pea <pea@misfist.com>
2022-07-27 20:27:07 -0400

    Formatting

--- footer.php
+++ footer.php
@@ -11,43 +11,46 @@
 
 ?>
 
-	<?php do_action( 'before_footer' ); ?>
-	
-	<footer class="site-footer">
+		<?php do_action( 'before_footer' ); ?>
+		
+		<footer class="site-footer">
 
-		<?php
-		if ( is_active_sidebar( 'footer' ) ) :
-			?>
-			<div class="footer-widgets widget-area">
-				<?php dynamic_sidebar( 'footer' ); ?>
-			</div><!-- .footer-widgets-->
 			<?php
-		endif;
-		?>
-
-		<nav id="site-footer-navigation" class="footer-navigation navigation-menu" aria-label="<?php esc_attr_e( 'Footer Navigation', 'the-territory' ); ?>">
-			<?php
-			wp_nav_menu(
-				array(
-					'fallback_cb'    => false,
-					'theme_location' => 'footer',
-					'menu_id'        => 'footer-menu',
-					'menu_class'     => 'menu container',
-					'container'      => false,
-					'depth'          => 1,
-				)
-			);
+			if ( is_active_sidebar( 'footer' ) ) :
+				?>
+				<div class="footer-widgets widget-area">
+					<?php dynamic_sidebar( 'footer' ); ?>
+				</div><!-- .footer-widgets-->
+				<?php
+			endif;
 			?>
-		</nav><!-- #site-navigation-->
 
-		<div class="container site-info">
-			<?php the_territory_display_copyright_text(); ?>
-			<?php the_territory_display_social_network_links(); ?>
-		</div><!-- .site-info -->
+			<nav id="site-footer-navigation" class="footer-navigation navigation-menu" aria-label="<?php esc_attr_e( 'Footer Navigation', 'the-territory' ); ?>">
+				<?php
+				wp_nav_menu(
+					array(
+						'fallback_cb'    => false,
+						'theme_location' => 'footer',
+						'menu_id'        => 'footer-menu',
+						'menu_class'     => 'menu container',
+						'container'      => false,
+						'depth'          => 1,
+					)
+				);
+				?>
+			</nav><!-- #site-navigation-->
+
+			<div class="container site-info">
+				<?php the_territory_display_copyright_text(); ?>
+				<?php the_territory_display_social_network_links(); ?>
+			</div><!-- .site-info -->
+
+		</footer><!-- .site-footer container-->
+
+		<?php the_territory_display_mobile_menu(); ?>
 
 	</main><!-- #main -->
 
-	<?php the_territory_display_mobile_menu(); ?>
 	<?php wp_footer(); ?>
 
 </body>
