079269e5d41d32debc8746532797c99f1ecdee79
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-04 14:38:01 -0400

    learn more content padding

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '556cb4aa6022936b6c79');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '8f9d51ce652f2ee85ce3');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'c760a8e3f86e03632b25');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'f08e4a00d03c2fa8b2de');
