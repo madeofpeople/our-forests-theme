5b0b8f2091559799b8e7bba9658373e4eb1e40f9
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-04 12:43:14 -0400

    tweaks to take action layouts / flows

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '25ea5b51208a22d9edc0');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '556cb4aa6022936b6c79');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '8b7f56e740fccd73142a');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'cbd7d583b86f0b808d0a');
