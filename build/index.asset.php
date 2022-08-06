a3a694bacc28908ea7edd69df352aff227d7de56
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-06 08:53:58 -0400

    header positioning tweaks for logged in desktop sizes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'e596a2ae21fed18c0f5a');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '75e90baf155ea6635cf4');
