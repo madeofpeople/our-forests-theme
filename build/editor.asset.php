cd097a0bcb0431d1c48eae6dfd6448c5b81536df
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-05 22:44:26 -0400

    laurel responsiveness improvements

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '0d9069c5204c84f28bdf');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'ca4d2268226e4f5fd2a0');
