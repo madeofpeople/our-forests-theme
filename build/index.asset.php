8acf00cd7f61a6ac7047bac253d89666c4a80f26
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-06 15:30:11 -0400

    icons in screening modal

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '93577777fee83eac6a5e');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '158554d33e316babd825');
