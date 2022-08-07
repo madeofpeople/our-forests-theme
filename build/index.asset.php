855950847c20db59b05ebeaecdb58c9444202ae2
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-07 11:33:36 -0400

    heading p alignment

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '0e063aa2255354920ddc');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '256d293fdd8de2b9995d');
