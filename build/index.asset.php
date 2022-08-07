1e271c83c26af8de95d066321a96442547969fdf
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-07 14:16:25 -0400

    404 and other base pages

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '781e3bc03d73a60ce06b');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '83cf0b0dd4dadfc1036f');
