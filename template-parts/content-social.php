04f7477b4c6a49057b1d05454e778d8b9b88fb6f
Pea <pea@misfist.com>
2022-08-08 14:53:50 -0400

    Update single social post template
    - Replace `.single-social.php` with `./template-parts/content-social.php`
    - Replace `get_field` with core `get_post_meta` function (won't produce fatal errors if ACF not active)
    - Fix download link
    - Added image id and classname

--- template-parts/content-social.php
+++ template-parts/content-social.php
@@ -8,41 +8,49 @@
  */
 
 ?>
-	<article <?php post_class( 'post-container' ); ?>>
+	<article <?php post_class( 'post-container section' ); ?>>
 
 		<header class="post-header">
-			<?php if ( has_post_thumbnail() ): ?>
-				<div class="<?php echo esc_attr( $base_class . '__post-thumbnail' ); ?>">
-					<a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_post_thumbnail( 'full' ) ?></a>
-				</div>
-			<?php endif; ?>
-			<?php the_title( '<h2 class="post-title">', '</h2>' ); ?>
+			<?php the_title( '<h2 class="post-title page__title">', '</h2>' ); ?>
 		</header><!-- .post-header -->
 
 		<div class="post-content">
 			<?php
-				the_content(
-					sprintf(
-						wp_kses(
-							/* translators: %s: Name of current post. */
-							esc_html__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'the-territory' ),
-							[
-								'span' => [
-									'class' => [],
-								],
-							]
-						),
-						the_title( '<span class="screen-reader-text">"', '"</span>', false )
-					)
-				);
-
-				wp_link_pages(
-					[
-						'before' => '<div class="page-links">' . esc_attr__( 'Pages:', 'the-territory' ),
-						'after'  => '</div>',
-					]
-				);
+			if ( $message = get_post_meta( get_the_id(), 'message', true ) ) :
 				?>
+				<?php echo apply_filters( 'the_content', $message ); ?>
+				<?php
+			endif;
+			?>
+
+			<?php
+			$images = \get_post_meta( get_the_ID(), 'images', true );
+			$size   = 'medium'; // (thumbnail, medium, large, full or custom size)
+			if ( ! empty( $images ) ) :
+				?>
+				<div class="social-image-grid">
+					<?php
+					foreach ( $images as $image_id ) :
+						?>
+						<div id="image-<?php echo $image_id; ?>" class="social-image-item">
+							<a href="<?php echo esc_url( wp_get_attachment_image_url( $image_id, $size ) ); ?>" target="_blank">
+								<?php echo \wp_get_attachment_image( $image_id, $size ); ?>
+							</a>
+						</div>
+
+						<?php
+					endforeach;
+					?>
+				</div><!-- .social-image-item -->
+			<?php endif; ?>
+
+			<?php
+			if ( $download = get_post_meta( get_the_ID(), 'download', true ) ) :
+				?>
+				<a class="wp-block-button__link" href="<?php echo esc_url( wp_get_attachment_url( $download ) ); ?>"><?php esc_html_e( 'Download Assets as a Zip File.', 'the-territory' ); ?></a>
+				<?php
+			endif;
+			?>
 		</div><!-- .post-content -->
 
 		<footer class="post-footer">
