35c363d6b7f0532d93010902e880ffe344fe6a7b
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-08 08:56:41 -0400

    some of spitzs requests

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '009e002155ef365c4f3e');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'd1a5defda5f65b742dcc');
