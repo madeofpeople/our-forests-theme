04a483bfeb7a0aa06059875d72acc119b6f05a48
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-14 14:51:28 -0400

    wrapup on social card tweaks

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '6165fd5d2201d4f45b1d');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '84fd447b68196ee11a94');
