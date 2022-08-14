95a6542dd506f4d8e8d8f28646bf2e87e9364fc0
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-14 14:22:28 -0400

    social post tweaks

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'a407ae322f67db054745');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '516caef43a94c78ea527');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'e30ccad221d7363512f0');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '144abced79d0fe5ea97d');
