0ea3119cf5f8bc72fe386c789ed6f5ea8660f0e6
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-15 12:09:32 -0400

    hide bp indicator

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'ba2ad59a7d36108e42ff');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '4549318252c96bd6c35c');
