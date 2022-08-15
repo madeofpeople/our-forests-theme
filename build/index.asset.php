7caed073db05e2fd338cfa62c3e2a4f70dcf6c73
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-15 01:27:02 -0400

    fix nav position when offscreen

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'eab703ca8724d4505cab');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '891f66904de4dd6a5fe3');
