a9ac1ea1ed35a2a7e4be531ad7df83c368be974d
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-15 02:02:58 -0400

    fixed nav tweaks across breakpoints

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '891f66904de4dd6a5fe3');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '58f902535a90355109af');
