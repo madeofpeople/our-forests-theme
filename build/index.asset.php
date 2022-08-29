06e8582e1fe67f216b82691d824cc94e3725fd71
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-08-29 10:04:52 -0400

    tweak to off screen page__nav top value

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '7836910e5e3f91cfb10a');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'aa4047d3c66070398c66');
