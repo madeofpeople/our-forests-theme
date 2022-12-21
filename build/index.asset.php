6b02187c915e8c6bda2445adbc9a5ff92814a452
Thiago de Mello Bueno <thiago@madeofpeople.org>
2022-12-20 23:54:21 -0500

    fixing list style in schedule a screening

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '1666e72345b5fdc10d79');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'bd680885d7ddef08255e');
