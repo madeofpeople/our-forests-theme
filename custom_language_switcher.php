fe5f5e2b616e47ba9658bd2092cc8db0db2d881d
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-01-13 18:00:01 -0500

    custom language switcher test

---
+++ custom_language_switcher.php
@@ -0,0 +1,14 @@
+<?php $array = trp_custom_language_switcher();  ?>
+<!-- IMPORTANT! You need to have data-no-translation on the wrapper with the links or TranslatePress will automatically translate them in a secondary language. -->
+<!--
+<ul data-no-translation>
+    <?php foreach ($array as $name => $item){ ?>
+            <li style="list-style-image: url(<?php echo $item['flag_link'] ?>)">
+                <a href="<?php echo $item['current_page_url']?>">
+                    <span><?php echo $item['short_language_name']. ':' . $item['language_name']?>
+                    </span>
+                </a>
+            </li>
+    <?php } ?>
+</ul>
+    -->
