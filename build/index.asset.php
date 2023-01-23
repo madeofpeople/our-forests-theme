4a4e843b810359ecf04fcda021e8d14ea26e3781
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-01-23 15:34:30 -0500

    translating work-with-us

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'f6fd31b070c4cf643984');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'fd4b19883f596cfaf9bf');
