3d41a8cf546b67faa3d564a09739d0b36cd396e8
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-11 12:20:57 -0400

    defender bits

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '5f22ac32cf4cd75afa8c');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'c2abe32965c50ed7ad69');
