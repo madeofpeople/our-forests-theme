fa4a4f060a4bc207e04bcbb5978cbd0fa2296a75
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-11 12:37:01 -0400

    watch teaser slug change

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'ecff49c5fc0416e44125');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '0a8dfc26d7c1609d3e58');
