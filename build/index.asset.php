f91ccd840cbf0a299a4f001dafdb24c961f94fb2
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-11 12:34:41 -0400

    defender background opacity

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '70ba7901f5549c17ff8a');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'ecff49c5fc0416e44125');
