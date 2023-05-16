f836687172c1401ef1257468a7540397078fb51a
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-16 14:58:30 -0400

    hero max width stuff

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'a824fe3f4d8501d8cf25');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '5b7fc4632660feb520d5');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '0fa3c0a2eb59be786059');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '827a7d8da3254659c242');
