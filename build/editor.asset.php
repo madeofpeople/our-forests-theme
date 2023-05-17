bd0c7cbdbba341400564eb40b7b80a6b4112182e
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-17 13:55:55 -0400

    support and drive styling changes

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '37eeb53d9ca477087c6a');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '405d58a34d6b4826b4b7');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '5d9768dc7f8b3835b35b');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'e0478d90866458fd09f2');
