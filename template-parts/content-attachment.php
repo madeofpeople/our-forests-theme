c2a993e0b4d085b8bcfea568fb13bf2fe22f5026
Pea <pea@misfist.com>
2023-05-17 22:10:47 -0400

    Add logic to select image size based on category

--- template-parts/content-attachment.php
+++ template-parts/content-attachment.php
@@ -15,12 +15,22 @@
 
 		<div class="entry-content">
 			<?php
-			
+			/**
+			 * Start: Do Not Modify
+			 */
 			if ( $attachment_url = \wp_get_attachment_image_url( $post->ID, 'full' ) ) :
 				$attachment_id = \attachment_url_to_postid( $attachment_url );
+				$image_size = 'large';
+				if( $media_category = wp_get_post_terms( $attachment_id, 'media_category' ) ) {
+					$media_category_slugs = wp_list_pluck( $media_category, 'slug' );
+					$image_size = sanitize_title_with_dashes( $media_category_slugs[0] . ' ' . $image_size );
+				}
+				/**
+				 * End: Do Not Modify
+				 */
 				?>
 				<figure class="post-image">
-					<?php echo \wp_get_attachment_image( $attachment_id, 'large' ); ?>
+					<?php echo \wp_get_attachment_image( $attachment_id, $image_size ); ?>
 					<?php
 					if( $attachment_caption = \wp_get_attachment_caption( $attachment_id ) ) :
 						?>
@@ -35,7 +45,9 @@
 			endif;
 			?>
 			<?php
-			echo \wp_kses_post( \wpautop( \get_the_content() ) );
+			// echo apply_filters( 'the_content', get_the_content() );
+			the_content();
+			// echo \wp_kses_post( \wpautop( \get_the_content() ) );
 			?>
 		</div><!-- .entry-content -->
 
