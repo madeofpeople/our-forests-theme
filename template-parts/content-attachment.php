e6c3321ad97265edfd8261cb9941553d7a7a8201
Pea <pea@misfist.com>
2023-05-17 23:31:51 -0400

    Update attachment template to include social links

--- template-parts/content-attachment.php
+++ template-parts/content-attachment.php
@@ -6,49 +6,48 @@
  *
  * @package Our Forests
  */
+if( ! $attachment_id = Our_Forests\get_attachment_media_id( $post->ID ) ) {
+	return;
+}
+$attributes = array(
+	'id'      => $attachment_id,
+	'url'     => wp_get_attachment_image_src( $attachment_id, 'full' ),
+	'message' => get_the_content( null, false, $post_id ),
+	'title'   => get_the_title( $post_id ),
+	'link'    => get_the_permalink( $post_id ),
+);
 ?>
-	<article <?php \post_class( 'post-container' ); ?>>
+	<article <?php \post_class( 'social-card' ); ?>>
 
 		<header class="entry-header">
-			<?php \the_title( '<h1 class="post-title page__title">', '</h1>' ); ?>
+			<?php \the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
 			<?php
-			/**
-			 * Start: Do Not Modify
-			 */
-			if ( $attachment_url = \wp_get_attachment_image_url( $post->ID, 'full' ) ) :
-				$attachment_id = \attachment_url_to_postid( $attachment_url );
-				$image_size = 'large';
-				if( $media_category = wp_get_post_terms( $attachment_id, 'media_category' ) ) {
-					$media_category_slugs = wp_list_pluck( $media_category, 'slug' );
-					$image_size = sanitize_title_with_dashes( $media_category_slugs[0] . ' ' . $image_size );
-				}
-				/**
-				 * End: Do Not Modify
-				 */
-				?>
-				<figure class="post-image">
-					<?php echo \wp_get_attachment_image( $attachment_id, $image_size ); ?>
-					<?php
-					if( $attachment_caption = \wp_get_attachment_caption( $attachment_id ) ) :
-						?>
-						<figcaption>
-							<?php echo \wp_kses_post( $attachment_caption ); ?>
-						</figcaption>
-						<?php
-					endif;
-					?>
-				</figure>
+			$image_size = 'large';
+			if( $media_category = wp_get_post_terms( $attachment_id, 'media_category' ) ) {
+				$media_category_slugs = wp_list_pluck( $media_category, 'slug' );
+				$image_size = sanitize_title_with_dashes( $media_category_slugs[0] . ' ' . $image_size );
+			}
+			?>
+			<figure class="post-image">
+				<?php echo \wp_get_attachment_image( $attachment_id, $image_size ); ?>
 				<?php
-			endif;
-			?>
-			<?php
-			// echo apply_filters( 'the_content', get_the_content() );
-			the_content();
-			// echo \wp_kses_post( \wpautop( \get_the_content() ) );
-			?>
+				if( $attachment_caption = \wp_get_attachment_caption( $attachment_id ) ) :
+					?>
+					<figcaption>
+						<?php echo \wp_kses_post( $attachment_caption ); ?>
+					</figcaption>
+					<?php
+				endif;
+				?>
+			</figure>
+		
+			<?php the_content();?>
+
+			<?php get_template_part( 'template-parts/components/share', 'links', array( 'attributes' => $attributes ) ); ?>
+
 		</div><!-- .entry-content -->
 
 		<footer class="entry-footer">
