baa38f6216ad64180402ac2e8b00c8ed39c34a9e
Pea <pea@misfist.com>
2023-05-17 23:32:25 -0400

    Helper functions

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -213,43 +213,9 @@ function our_forests_the_content( $excluded_blocks = array() ) {
  * @param array $args The parameters needed to get the SVG.
  */
 function our_forests_display_svg( $args = array() ) {
-	$kses_defaults = wp_kses_allowed_html( 'post' );
-
-	$svg_args = array(
-		'svg'   => array(
-			'class'           => true,
-			'aria-hidden'     => true,
-			'aria-labelledby' => true,
-			'role'            => true,
-			'xmlns'           => true,
-			'width'           => true,
-			'height'          => true,
-			'viewbox'         => true, // <= Must be lower case!
-			'color'           => true,
-			'stroke-width'    => true,
-		),
-		'g'     => array( 'color' => true ),
-		'title' => array(
-			'title' => true,
-			'id'    => true,
-		),
-		'path'  => array(
-			'd'     => true,
-			'color' => true,
-		),
-		'use'   => array(
-			'xlink:href' => true,
-		),
-	);
-
-	$allowed_tags = array_merge(
-		$kses_defaults,
-		$svg_args
-	);
-
 	echo wp_kses(
 		our_forests_get_svg( $args ),
-		$allowed_tags
+		Our_Forests\get_kses_svg_ruleset()
 	);
 }
 
