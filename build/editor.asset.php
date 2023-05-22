43181c3c9a93b9c86539980c508c59ddc237d60e
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-22 18:24:42 -0400

    min height for social posts

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'c8b722c2081940fc9b17');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'bfe51df94b15d036d897');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '30736ff0e4a4be6bd04e');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '43d6185ba26b68711cd8');
