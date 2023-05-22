c498b6b691a7e74fadd8fc311cf7123f02698d5a
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-22 18:06:09 -0400

    social card grid selectors

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '33e49deda1218efa80df');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'c8b722c2081940fc9b17');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '8972341bade13cf6be01');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '30736ff0e4a4be6bd04e');
