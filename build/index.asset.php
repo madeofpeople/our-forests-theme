155d67df877ed7a0a96c88a03714015f913ab8be
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-23 11:16:22 -0400

    support responsiveness tweak

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '7c0237b43d5cb87a94b8');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '6825b878023460937dfc');
