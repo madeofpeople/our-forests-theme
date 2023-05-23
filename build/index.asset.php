43aeab9ccdbd985f8dfa87b2b2fb936ac0fee70d
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-23 12:36:15 -0400

    page-nav tweaks

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '6825b878023460937dfc');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'd47ea4e161bc7dd06421');
