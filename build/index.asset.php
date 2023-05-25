a3b5c866945d20d1feb0c552d176d7f5afae68da
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-25 10:32:03 -0400

    social actions in content attachment

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '42785c31a9684023e065');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '544c64896f8590a117d9');
