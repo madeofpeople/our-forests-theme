7bcf525978e6a9b96c82f6e28f4f3f420c1b31e8
Pea <pea@misfist.com>
2023-05-25 13:05:50 -0400

    Replace hardcoded dummy text with `intro_text` field

--- template-parts/content-attachment.php
+++ template-parts/content-attachment.php
@@ -26,9 +26,15 @@ $attributes = array(
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
-			<p class="introp">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
+			<?php
+			if( $intro_text = get_post_meta( $post_id, 'intro_text', true ) ) :
+				?>
+				<div class="intro lede">
+					<?php echo apply_filters( 'the_content', $intro_text ); ?>
+				</div>
+				<?php
+			endif;
+			?>
 			<?php
 			$image_size = 'large';
 			if( $media_category = wp_get_post_terms( $attachment_id, 'media_category' ) ) {
