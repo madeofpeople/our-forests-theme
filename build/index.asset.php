2a7b95fd78eeacbc19027f07d5daf41cf0f10c11
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-25 11:13:13 -0400

    margin on social header for content attachment

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '6d95a5fbc43e60eeb9d3');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '57cfb6c3ce0ec04b7359');
