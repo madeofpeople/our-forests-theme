882bc93b6930a3863577cb40ffaf7d8ad4c81a84
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-26 17:48:02 -0400

    card mobile tweaks

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '57cfb6c3ce0ec04b7359');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '35e8ce0f33854389c945');
