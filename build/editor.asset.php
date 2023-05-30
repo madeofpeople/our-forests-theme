3e48080e8e78fb9c00f64ae842b9d83955f716ac
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-30 13:44:57 -0400

    link and svg colors in forest profile share links

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '72304eb9ec6163fe0601');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '9cd8712cd4ff40f87bdb');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '0d582e4cc3e53878d207');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '9a6e56d71c4e1b3fb0c4');
