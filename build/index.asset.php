942ed4655d250ddd7975073c21c5bacae0973c03
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-30 17:17:28 -0400

    heading margins

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '27f3d04ea8ba1f89125e');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'd1f48377345c7282c947');
