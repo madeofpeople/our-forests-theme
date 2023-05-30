a93214a99af9691bf2a1051482ba5d83172fb71a
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-30 12:03:33 -0400

    social card mobile themeing, and some heading things for smaller breakpoints

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '735582a5a3e1aa88013c');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '72304eb9ec6163fe0601');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '35e8ce0f33854389c945');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '0d582e4cc3e53878d207');
