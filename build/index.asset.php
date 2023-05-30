cff8c45a59b38a05e1afbe8870216b850520d7c0
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-30 17:08:29 -0400

    last min tweak

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '150d2af36ba3fd1a4121');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '27f3d04ea8ba1f89125e');
