7e421da12060604b62c45ee90f06faa22aed81d6
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-05-30 13:58:36 -0400

    heading changes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '9a6e56d71c4e1b3fb0c4');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'fdfb532a804f503e39bc');
