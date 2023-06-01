a12640f720bfac4b6935a131dc7a87b7ecfcdb26
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-06-01 12:36:16 -0400

    social card overflow-hidden tweaks

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '9cd8712cd4ff40f87bdb');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'c5218de55e087d9c29a8');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'd1f48377345c7282c947');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '7e739c02bedc7c06fecc');
