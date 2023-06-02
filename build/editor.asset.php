3cf0d478d9a7fef6d3134cf6a4af191b6cf1e9bf
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-06-02 13:24:50 -0400

    alignment of forests in defender profile

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '033461bdc5fcc21f1a6c');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '8977d5e08f49be913a46');
