e347eb486eea1a475862037093644b0064942d44
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-06-02 16:06:40 -0400

    responsiveness re: campaign goals

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '8977d5e08f49be913a46');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'e3d62b04aac49c04d561');
