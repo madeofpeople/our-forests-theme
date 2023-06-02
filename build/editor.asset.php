10217b7930a7eeb4caec27b9536b40d028e44811
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-06-02 13:15:22 -0400

    equal heights campaign goals

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '7ca9330a4bcd1db0e1c9');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '033461bdc5fcc21f1a6c');
