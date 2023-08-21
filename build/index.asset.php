b98b453eead1bd992d8c35ae8e0f503660610309
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-08-21 17:05:24 -0400

    watch-the-series

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '03ae20923230fbb0d147');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'fb683d31b981a5cca483');
