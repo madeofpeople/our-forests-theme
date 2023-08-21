a7b70069efc325fcb86d3da071a8450379bdc35f
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-08-21 12:27:31 -0400

    styling changes for episodes page, and v2 vision

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '16edb8e82bdd1da4335b');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '03ae20923230fbb0d147');
