274403fc185e0cc8ed05d27f9a804cb6783d6362
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-09-01 16:40:49 -0400

    share centereing

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '7cdd321f64219753d9b3');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '330da7f1c30c2ba0b367');
