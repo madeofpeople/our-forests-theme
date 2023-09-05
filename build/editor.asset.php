613053a836174c6ade7fc65aeb03f3820ec9d6c9
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-09-05 11:16:32 -0400

    defenders tweaks

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'd6ed71816a91a1ac3cc0');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '8a23fdc6e941d5712121');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '9ff733ed1e72b0f8701a');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'dea17bc0ef1822d798a6');
