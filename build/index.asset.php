e6d93b9fb6b731795fddd1b42ebac8f7b0227a7d
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-09-13 16:59:58 -0400

    more theme support

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'ecf2c2f329c89ceb1b62');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '3d31f33a060c597d8665');
