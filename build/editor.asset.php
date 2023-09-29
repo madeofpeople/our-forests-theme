0e6245d0e8d0ae06d33e3a2134e96fed1f700cdb
Thiago de Mello Bueno <thiago@madeofpeople.org>
2023-09-29 08:57:52 -0400

    fix for overlap on homepage

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'a0ac0221c199469d2fae');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '5432219ceb6c04b1e345');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '9fb0912b3906600e8827');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '37cad728e488077bc753');
